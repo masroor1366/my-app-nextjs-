8e0e7f14a80de129b4b8510e61d37ed9dfdec10a
masroor <masroor.1388@gmail.com>
2024-03-27 00:55:06 +0330

    fix error

---
+++ app/layuots/page.js
@@ -0,0 +1,22 @@
+ 
+ 
+
+
+
+import Dashboard from "../dashboard/Dashboard";
+
+ 
+
+ 
+export default function Home() {
+  return (
+    
+   
+   <section id="content_section" 
+    
+   className={`bg-light py-2 px-3  `}>
+     <Dashboard/>
+   </section>
+     
+  );
+}
